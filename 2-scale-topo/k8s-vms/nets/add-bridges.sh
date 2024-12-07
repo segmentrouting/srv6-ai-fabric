b2cb66786dc4b753c9ba5f8a57753b4cd1de58d0
brmcdoug <brmcdoug@cisco.com>
2024-12-06 20:21:14 -0800

    nets

--- 2-scale-topo/k8s-vms/nets/add-bridges.sh
+++ 2-scale-topo/k8s-vms/nets/add-bridges.sh
@@ -1,5 +1,39 @@
 #!/bin/bash
 
+virsh net-define cp-node00-0.xml
+virsh net-define cp-node00-1.xml
+virsh net-define cp-node00-2.xml
+virsh net-define cp-node00-3.xml
+virsh net-define wkr-node01-0.xml
+virsh net-define wkr-node01-1.xml
+virsh net-define wkr-node01-2.xml
+virsh net-define wkr-node01-3.xml
+virsh net-define wkr-node02-0.xml
+virsh net-define wkr-node02-1.xml
+virsh net-define wkr-node02-2.xml
+virsh net-define wkr-node02-3.xml
+virsh net-define wkr-node03-0.xml
+virsh net-define wkr-node03-1.xml
+virsh net-define wkr-node03-2.xml
+virsh net-define wkr-node03-3.xml
+
+virsh net-start cp-node00-0
+virsh net-start cp-node00-1
+virsh net-start cp-node00-2
+virsh net-start cp-node00-3
+virsh net-start wkr-node01-0
+virsh net-start wkr-node01-1
+virsh net-start wkr-node01-2
+virsh net-start wkr-node01-3
+virsh net-start wkr-node02-0
+virsh net-start wkr-node02-1
+virsh net-start wkr-node02-2
+virsh net-start wkr-node02-3
+virsh net-start wkr-node03-0
+virsh net-start wkr-node03-1
+virsh net-start wkr-node03-2
+virsh net-start wkr-node03-3
+
 sudo brctl addbr cp-node00-0
 sudo brctl addbr cp-node00-1
 sudo brctl addbr cp-node00-2
