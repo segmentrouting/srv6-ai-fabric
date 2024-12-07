4700005bc6fc553f85f9be485a89df96fffd479d
brmcdoug <brmcdoug@cisco.com>
2024-12-06 20:05:38 -0800

    nets

--- 2-scale-topo/k8s-vms/nets/add-bridges.sh
+++ 2-scale-topo/k8s-vms/nets/add-bridges.sh
@@ -1,11 +1,35 @@
 #!/bin/bash
 
-sudo brctl addbr k8s-cp-node00
-sudo brctl addbr k8s-wkr-node01
-sudo brctl addbr k8s-wkr-node02
-sudo brctl addbr k8s-wkr-node03
+sudo brctl addbr k8s-cp-node00-0
+sudo brctl addbr k8s-cp-node00-1
+sudo brctl addbr k8s-cp-node00-2
+sudo brctl addbr k8s-cp-node00-3
+sudo brctl addbr k8s-wkr-node01-0
+sudo brctl addbr k8s-wkr-node01-1
+sudo brctl addbr k8s-wkr-node01-2
+sudo brctl addbr k8s-wkr-node01-3
+sudo brctl addbr k8s-wkr-node02-0
+sudo brctl addbr k8s-wkr-node02-1
+sudo brctl addbr k8s-wkr-node02-2
+sudo brctl addbr k8s-wkr-node02-3
+sudo brctl addbr k8s-wkr-node03-0
+sudo brctl addbr k8s-wkr-node03-1
+sudo brctl addbr k8s-wkr-node03-2
+sudo brctl addbr k8s-wkr-node03-3
 
-sudo ip link set dev k8s-cp-node00 up
-sudo ip link set dev k8s-wkr-node01 up
-sudo ip link set dev k8s-wkr-node02 up
-sudo ip link set dev k8s-wkr-node03 up
+sudo ip link set dev k8s-cp-node00-0 up
+sudo ip link set dev k8s-cp-node00-1 up
+sudo ip link set dev k8s-cp-node00-2 up
+sudo ip link set dev k8s-cp-node00-3 up
+sudo ip link set dev k8s-wkr-node01-0 up
+sudo ip link set dev k8s-wkr-node01-1 up
+sudo ip link set dev k8s-wkr-node01-2 up
+sudo ip link set dev k8s-wkr-node01-3 up
+sudo ip link set dev k8s-wkr-node02-0 up
+sudo ip link set dev k8s-wkr-node02-1 up
+sudo ip link set dev k8s-wkr-node02-2 up
+sudo ip link set dev k8s-wkr-node02-3 up
+sudo ip link set dev k8s-wkr-node03-0 up
+sudo ip link set dev k8s-wkr-node03-1 up
+sudo ip link set dev k8s-wkr-node03-2 up
+sudo ip link set dev k8s-wkr-node03-3 up
