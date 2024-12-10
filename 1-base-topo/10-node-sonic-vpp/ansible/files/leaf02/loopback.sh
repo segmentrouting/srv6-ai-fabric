48f953507af09111b3bb3ec13b76976b63020a6f
brmcdoug <brmcdoug@cisco.com>
2024-12-10 13:19:36 -0800

    ansible

---
+++ 1-base-topo/10-node-sonic-vpp/ansible/files/leaf02/loopback.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+sudo config interface ip add Loopback0 10.0.0.8/32
+sudo config interface ip add Loopback0 fc00:0:8::1/128
---
+++ 1-base-topo/10-node-sonic-vpp/ansible/files/leaf03/loopback.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+sudo config interface ip add Loopback0 10.0.0.9/32
+sudo config interface ip add Loopback0 fc00:0:9::1/128
