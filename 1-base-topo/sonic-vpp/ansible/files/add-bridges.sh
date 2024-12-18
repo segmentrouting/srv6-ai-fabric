89efc8fd85027a41e581babf80792978373dffdd
brmcdoug <brmcdoug@cisco.com>
2024-12-18 12:26:41 -0800

    typo

---
+++ 1-base-topo/sonic-vpp/ansible/files/add-bridges.sh
@@ -0,0 +1,18 @@
+#! /bin/bash
+
+sudo brctl addbr br0
+sudo brctl addbr br1
+sudo brctl addbr br2
+sudo brctl addbr br3
+sudo brctl addbr br4
+sudo brctl addbr br5
+sudo brctl addbr br6
+sudo brctl addbr br7
+sudo brctl addbr br8
+sudo brctl addbr br9
+sudo brctl addbr br10
+sudo brctl addbr br11
+sudo brctl addbr br12
+sudo brctl addbr br13
+sudo brctl addbr br14
+sudo brctl addbr br15
