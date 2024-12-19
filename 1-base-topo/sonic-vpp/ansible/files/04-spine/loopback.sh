8f29db31505ee3c511dd868996a1763124299937
brmcdoug <brmcdoug@cisco.com>
2024-12-18 19:39:50 -0800

    loopbacks

--- 1-base-topo/sonic-vpp/ansible/files/04-spine/loopback.sh
+++ 1-base-topo/sonic-vpp/ansible/files/04-spine/loopback.sh
@@ -2,3 +2,6 @@
 
 sudo config interface ip add Loopback0 10.0.0.4/32
 sudo config interface ip add Loopback0 fc00:0:4::1/128
+sudo ip link add sr0 type dummy
+sudo ip link set sr0 up 
+sudo config save -y 
