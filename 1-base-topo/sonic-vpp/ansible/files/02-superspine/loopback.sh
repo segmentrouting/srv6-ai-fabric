e417f1bed48a71d13e814016b0e484c13b56bf0e
brmcdoug <brmcdoug@cisco.com>
2024-12-19 14:38:01 -0800

    sonic subset and no bgp import check

--- 1-base-topo/sonic-vpp/ansible/files/02-superspine/loopback.sh
+++ 1-base-topo/sonic-vpp/ansible/files/02-superspine/loopback.sh
@@ -4,4 +4,8 @@ sudo config interface ip add Loopback0 10.0.0.2/32
 sudo config interface ip add Loopback0 fc00:0:2::1/128
 sudo ip link add sr0 type dummy
 sudo ip link set sr0 up 
+sudo sysctl -w net.vrf.strict_mode=1
+sudo sysctl -w net.ipv6.seg6_flowlabel=1
+sudo sysctl -w net.ipv6.fib_multipath_hash_policy=3
+sudo sysctl -w net.ipv6.fib_multipath_hash_fields=11
 sudo config save -y 
