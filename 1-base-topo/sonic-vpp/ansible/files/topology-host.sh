84e7f6de5186945fdcdac3cedd0f874093835c49
brmcdoug <brmcdoug@cisco.com>
2024-12-20 12:51:39 -0800

    readme, sh script

--- 1-base-topo/sonic-vpp/ansible/files/topology-host.sh
+++ 1-base-topo/sonic-vpp/ansible/files/topology-host.sh
@@ -1,21 +1,20 @@
 #! /bin/bash
 
-#ip -6 route add fc00:0:f800:6::/64 encap seg6 mode encap segs fc00:0:4:2:6:10:fe08:: dev br16
-
+# Create namespace and veth pair for 09-leaf Ethernet8 "client" connection
 sudo ip netns add 09-host
-sudo ip link add veth09 type veth peer name veth21
+sudo ip link add veth09 type veth peer name veth20
 
 # Move veth09 to namespace 09-host
 sudo ip link set veth09 netns 09-host
 
-# Connect veth21 to bridge br21
-sudo brctl addif br21 veth21
+# Connect the other side veth20 to bridge br20, which is connected to 09-leaf Ethernet8
+sudo brctl addif br20 veth20
 
 # Bring up the interfaces
 sudo ip netns exec 09-host ip link set veth09 up
-sudo ip link set veth21 up
+sudo ip link set veth20 up
 
-# leaf09 Ethernet8 container
+# Spin up an alpine container to connet to 09-leaf Ethernet8
 docker run -i -t --name 09-leaf-08 iejalapeno/alpine:latest /bin/sh 
 PID1=$(sudo docker inspect -f '{{.State.Pid}}' 09-leaf-08)
 sudo ln -s /proc/$PID1/ns/net /var/run/netns/$PID1
@@ -25,6 +24,6 @@ sudo brctl addif br20 09l08
 sudo ip netns exec $PID1 ip addr add fc00:0:f800:4::2/64 dev 09h08
 sudo ip netns exec $PID1 ip link set 09h08 up
 sudo ip link set 09l08 up
-sudo ip netns exec $PID1 ping fc00:0:f800:4::1
+sudo ip netns exec $PID1 ping fc00:0:f800:4::1 -c 2
 sudo ip netns exec $PID1 ip -6 route add fc00:0:f800::/48 via fc00:0:f800:4::1 dev 09h08
 sudo ip netns exec $PID1 ip -6 route
--- 1-base-topo/sonic-vpp/readme.md
+++ 1-base-topo/sonic-vpp/readme.md
@@ -10,4 +10,38 @@
 sudo vxr.py start <filename>
 ```
 
+3. It can take up to 7 or 8 minutes for all nodes to come up and SWSS and FRR be ready
+   
+4. Run Ansible sonic-vpp playbook to configre nodes (config_db.json and frr.conf). Note, the playbook also creates user cisco with password cisco123 and sudo permission for easier login to the devices.
 
+```
+cd ansible
+ansible-playbook sonic-vpp-playbook.yml
+```
+
+5. Ansible playbook to deploy frr SRv6 configuration - hopefully this step will be removed in the future
+```
+cd ansible
+ansible-playbook sonic-srv6-playbook.yml
+```
+
+### Troubleshooting
+
+Accessing VPP 
+```
+docker exec -it syncd bash
+vppctl -s /run/vpp/cli.sock
+```
+
+VPP commands
+```
+show sr localsids
+show sr policies
+show sr steering-policies
+show sr encaps source addr 
+```
+
+SAI Redis logs
+```
+sudo tail -f /var/log/syslog /var/log/swss/sairedis.rec 
+```
