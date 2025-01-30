4c06ef9706ee6b9ba1e417c68d20ad5e3451251f
brmcdoug <brmcdoug@cisco.com>
2025-01-29 20:41:25 -0800

    trex

--- 20-node-topo/trex/trex-setup.sh
+++ 20-node-topo/trex/trex-setup.sh
@@ -6,44 +6,44 @@
 docker exec -it clab-clos-host00 ip addr add 10.10.0.2/24 dev eth1
 docker exec -it clab-clos-host00 ip addr add fc00:0:f800::2/64 dev eth1
 docker exec -it clab-clos-host00 ip -6 route add fc00:0:fe00::/48 via fc00:0:f800::1 dev eth1
-docker exec -it clab-clos-host00 ip -6 route add fc00:0:f800:8000::/64 encap seg6 mode encap segs fc00:0:fe00:fe00:fe04:fe00:: dev eth1
+docker exec -it clab-clos-host00 ip -6 route add fc00:0:f800:8000::/64 encap seg6 mode encap segs fc00:0:fe00:fe00:fe04:fe04:: dev eth1
 
 docker exec -it clab-clos-host00 ip addr add 10.10.4.2/24 dev eth2
 docker exec -it clab-clos-host00 ip addr add fc00:0:f800:4::2/64 dev eth2
 docker exec -it clab-clos-host00 ip -6 route add fc00:0:fe01::/48 via fc00:0:f800:4::1 dev eth2
-docker exec -it clab-clos-host00 ip -6 route add fc00:0:f800:8004::/64 encap seg6 mode encap segs fc00:0:fe01:fe01:fe05:fe01:: dev eth2
+docker exec -it clab-clos-host00 ip -6 route add fc00:0:f800:8004::/64 encap seg6 mode encap segs fc00:0:fe01:fe01:fe05:fe05:: dev eth2
 
 docker exec -it clab-clos-host00 ip addr add 10.10.8.2/24 dev eth3
 docker exec -it clab-clos-host00 ip addr add fc00:0:f800:8::2/64 dev eth3
 docker exec -it clab-clos-host00 ip -6 route add fc00:0:fe02::/48 via fc00:0:f800:8::1 dev eth3
-docker exec -it clab-clos-host00 ip -6 route add fc00:0:f800:8008::/64 encap seg6 mode encap segs fc00:0:fe02:fe02:fe06:fe02:: dev eth3
+docker exec -it clab-clos-host00 ip -6 route add fc00:0:f800:8008::/64 encap seg6 mode encap segs fc00:0:fe02:fe02:fe06:fe06:: dev eth3
 
 docker exec -it clab-clos-host00 ip addr add 10.10.12.2/24 dev eth4
 docker exec -it clab-clos-host00 ip addr add fc00:0:f800:c::2/64 dev eth4
 docker exec -it clab-clos-host00 ip -6 route add fc00:0:fe03::/48 via fc00:0:f800:c::1 dev eth4
-docker exec -it clab-clos-host00 ip -6 route add fc00:0:f800:800c::/64 encap seg6 mode encap segs fc00:0:fe03:fe03:fe07:fe03:: dev eth4
+docker exec -it clab-clos-host00 ip -6 route add fc00:0:f800:800c::/64 encap seg6 mode encap segs fc00:0:fe03:fe03:fe07:fe07:: dev eth4
 
 
 ## host01
 docker exec -it clab-clos-host01 ip addr add 10.10.1.2/24 dev eth1
 docker exec -it clab-clos-host01 ip addr add fc00:0:f800:1::2/64 dev eth1
 docker exec -it clab-clos-host01 ip -6 route add fc00:0:fe01::/48 via fc00:0:f800:1::1 dev eth1
-docker exec -it clab-clos-host01 ip -6 route add fc00:0:f800:8001::/64 encap seg6 mode encap segs fc00:0:fe01:fe00:fe05:fe00:: dev eth1
+docker exec -it clab-clos-host01 ip -6 route add fc00:0:f800:8001::/64 encap seg6 mode encap segs fc00:0:fe01:fe00:fe05:fe04:: dev eth1
 
 docker exec -it clab-clos-host01 ip addr add 10.10.5.2/24 dev eth2
 docker exec -it clab-clos-host01 ip addr add fc00:0:f800:5::2/64 dev eth2
 docker exec -it clab-clos-host01 ip -6 route add fc00:0:fe02::/48 via fc00:0:f800:5::1 dev eth2
-docker exec -it clab-clos-host01 ip -6 route add fc00:0:f800:8005::/64 encap seg6 mode encap segs fc00:0:fe02:fe01:fe06:fe01:: dev eth2
+docker exec -it clab-clos-host01 ip -6 route add fc00:0:f800:8005::/64 encap seg6 mode encap segs fc00:0:fe02:fe01:fe06:fe05:: dev eth2
 
 docker exec -it clab-clos-host01 ip addr add 10.10.9.2/24 dev eth3
 docker exec -it clab-clos-host01 ip addr add fc00:0:f800:9::2/64 dev eth3
 docker exec -it clab-clos-host01 ip -6 route add fc00:0:fe03::/48 via fc00:0:f800:9::1 dev eth3
-docker exec -it clab-clos-host01 ip -6 route add fc00:0:f800:8009::/64 encap seg6 mode encap segs fc00:0:fe03:fe02:fe07:fe02:: dev eth3
+docker exec -it clab-clos-host01 ip -6 route add fc00:0:f800:8009::/64 encap seg6 mode encap segs fc00:0:fe03:fe02:fe07:fe06:: dev eth3
 
 docker exec -it clab-clos-host01 ip addr add 10.10.13.2/24 dev eth4
 docker exec -it clab-clos-host01 ip addr add fc00:0:f800:d::2/64 dev eth4
 docker exec -it clab-clos-host01 ip -6 route add fc00:0:fe00::/48 via fc00:0:f800:d::1 dev eth4
-docker exec -it clab-clos-host01 ip -6 route add fc00:0:f800:800d::/64 encap seg6 mode encap segs fc00:0:fe00:fe03:fe04:fe03:: dev eth4
+docker exec -it clab-clos-host01 ip -6 route add fc00:0:f800:800d::/64 encap seg6 mode encap segs fc00:0:fe00:fe03:fe04:fe07:: dev eth4
 
 
 ## host02
@@ -60,10 +60,10 @@ docker exec -it clab-clos-host02 ip -6 route add fc00:0:f800:4::/64 encap seg6 m
 docker exec -it clab-clos-host02 ip addr add 10.10.24.2/24 dev eth3
 docker exec -it clab-clos-host02 ip addr add fc00:0:f800:8008::2/64 dev eth3
 docker exec -it clab-clos-host02 ip -6 route add fc00:0:fe02::/48 via fc00:0:f800:8008::1 dev eth3
-docker exec -it clab-clos-host02 ip -6 route add fc00:0:f800:8008::/64 encap seg6 mode encap segs fc00:0:fe02:fe02:fe02:fe02:: dev eth3
+docker exec -it clab-clos-host02 ip -6 route add fc00:0:f800:8::/64 encap seg6 mode encap segs fc00:0:fe02:fe02:fe02:fe02:: dev eth3
 
 docker exec -it clab-clos-host02 ip addr add 10.10.28.2/24 dev eth4
-docker exec -it clab-clos-host02 ip addr add fc00:0:f800:e::2/64 dev eth4
+docker exec -it clab-clos-host02 ip addr add fc00:0:f800:800c::2/64 dev eth4
 docker exec -it clab-clos-host02 ip -6 route add fc00:0:fe03::/48 via fc00:0:f800:800c::1 dev eth4
 docker exec -it clab-clos-host02 ip -6 route add fc00:0:f800:c::/64 encap seg6 mode encap segs fc00:0:fe03:fe03:fe03:fe03:: dev eth4
 
@@ -90,13 +90,26 @@ docker exec -it clab-clos-host03 ip -6 route add fc00:0:f800:d::/64 encap seg6 m
 
 ### Deletes
 
-# ip addr del fc00:0:f800::2/64 dev eth1
-# ip -6 route del fc00:0:fe00::/48 via fc00:0:f800::1 dev eth1
-# ip route del 10.10.16.0/24 encap seg6 mode encap segs fc00:0:fe00:fe00:fe04:fe04:: dev eth1
-# ip -6 route del fc00:0:f800:8000::/64 encap seg6 mode encap segs fc00:0:fe00:fe00:fe04:fe04:: dev eth1
+docker exec -it clab-clos-host00 ip -6 route del fc00:0:f800:8000::/64
+docker exec -it clab-clos-host00 ip -6 route del fc00:0:f800:8004::/64
+docker exec -it clab-clos-host00 ip -6 route del fc00:0:f800:8008::/64
+docker exec -it clab-clos-host00 ip -6 route del fc00:0:f800:800c::/64
 
-# ip addr del 10.10.4.2/24 dev eth2
-# ip addr del fc00:0:f800:4::2/64 dev eth2
-# ip -6 route del fc00:0:fe01::/48 via fc00:0:f800:4::1 dev eth2
-# ip route del 10.10.20.0/24 encap seg6 mode encap segs fc00:0:fe00:fe00:fe04:fe04:: dev eth2
-# ip -6 route del fc00:0:f800:8004::/64 encap seg6 mode encap segs fc00:0:fe01:fe01:fe05:fe05:: dev eth2
+docker exec -it clab-clos-host01 ip -6 route del fc00:0:f800:8001::/64
+docker exec -it clab-clos-host01 ip -6 route del fc00:0:f800:8005::/64
+docker exec -it clab-clos-host01 ip -6 route del fc00:0:f800:8009::/64
+docker exec -it clab-clos-host01 ip -6 route del fc00:0:f800:800d::/64
+
+docker exec -it clab-clos-host02 ip -6 route del fc00:0:f800::/64
+docker exec -it clab-clos-host02 ip -6 route del fc00:0:f800:4::/64
+docker exec -it clab-clos-host02 ip -6 route del fc00:0:f800:8::/64
+docker exec -it clab-clos-host02 ip -6 route del fc00:0:f800:c::/64
+
+docker exec -it clab-clos-host03 ip -6 route del fc00:0:f800:1::/64
+docker exec -it clab-clos-host03 ip -6 route del fc00:0:f800:5::/64
+docker exec -it clab-clos-host03 ip -6 route del fc00:0:f800:9::/64
+docker exec -it clab-clos-host03 ip -6 route del fc00:0:f800:d::/64
+
+
+
+
