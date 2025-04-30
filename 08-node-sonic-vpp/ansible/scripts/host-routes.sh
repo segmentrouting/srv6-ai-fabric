614bb2a0c09b990e9a4b1b6079293d3f679b4628
brmcdoug <brmcdoug@cisco.com>
2025-04-30 13:52:41 -0700

    typo

--- 08-node-sonic-vpp/ansible/scripts/host-routes.sh
+++ 08-node-sonic-vpp/ansible/scripts/host-routes.sh
@@ -23,12 +23,12 @@ docker exec -it clab-sonic-host08 ip addr add 200.8.100.2/24 dev eth1
 docker exec -it clab-sonic-host08 ip addr add 2001:db8:1008:0::2/64 dev eth1
 docker exec -it clab-sonic-host08 ip -6 route add fc00:0::/32 via 2001:db8:1008:0::1 dev eth1
 
-docker exec -it clab-sonic-host08 ip route add 200.0.100.0/24 encap seg6 mode encap segs fc00:0:1001:1201:fe00:: dev eth1
+docker exec -it clab-sonic-host08 ip route add 200.0.100.0/24 encap seg6 mode encap segs fc00:0:1001:1200:fe00:: dev eth1
 docker exec -it clab-sonic-host08 ip route add 200.16.100.0/24 encap seg6 mode encap segs fc00:0:1002:1202:fe00:: dev eth1
 docker exec -it clab-sonic-host08 ip route add 200.24.100.0/24 encap seg6 mode encap segs fc00:0:1003:1203:fe00:: dev eth1
 docker exec -it clab-sonic-host08 ip route
 
-docker exec -it clab-sonic-host08 ip -6 route add 2001:db8:1000::/64 encap seg6 mode encap segs fc00:0:1001:1200:: dev eth1
+docker exec -it clab-sonic-host08 ip -6 route add 2001:db8:1000::/64 encap seg6 mode encap segs fc00:0:1000:1200:: dev eth1
 docker exec -it clab-sonic-host08 ip -6 route add 2001:db8:1016::/64 encap seg6 mode encap segs fc00:0:1002:1202:: dev eth1
 docker exec -it clab-sonic-host08 ip -6 route add 2001:db8:1024::/64 encap seg6 mode encap segs fc00:0:1003:1203:: dev eth1
 docker exec -it clab-sonic-host08 ip -6 route
